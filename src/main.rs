91527c540b8177e9fc4b552d8b7e10a741bb01a9
kitaya <kkitaya@nzigen.com>
2024-01-27 23:18:28 +0900

    modify: routing with macro

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_files as fs;
 use actix_web::{web, App, HttpServer, Responder, get, post, HttpResponse};
+use actix_web::web::resource;
 use tera::Tera;
 
 // Self modules
@@ -14,11 +15,24 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let tera = Tera::new("template/**/*").expect("Error parsing templates");
         // fs::Files::new("{mount_point}", "{relative directory}")
-        App::new().app_data(web::Data::new(tera.clone()))
+        App::new()
             .service(fs::Files::new("/static", "./static").show_files_listing())
-            .service(web::scope("/")
-                .route("", web::get().to(view::general::index))
+            .service(
+                // warning: If implementing web::scope() as root directory => web::scope("/")
+                // you can only access as following path.
+                // .route("skills", web::get().to(view::general::skills))
+                // http://{hostname}// (<= unexpected slash) skills
+                web::scope("/app")
+            //         .route("", web::get().to(view::general::index))
+            //         .route("introduce", web::get().to(view::general::introduce))
+            //         .route("products", web::get().to(view::general::products))
+            //         .route("skills", web::get().to(view::general::skills))
+            .service(view::general::index)
+            .service(view::general::introduce)
+            .service(view::general::products)
+            .service(view::general::skills)
             )
+            .app_data(web::Data::new(tera.clone()))
             .default_service(web::route().to(view::general::not_found))
     })
         .bind(("127.0.0.1", 8080))?
--- src/view/general.rs
+++ src/view/general.rs
@@ -1,14 +1,50 @@
-use actix_web::{Responder, HttpResponse};
+use actix_web::{Responder, HttpResponse,get};
 use actix_web::web::Data;
 use tera::Tera;
 
+#[get("")]
 pub async fn index(tera: Data<Tera>) -> impl Responder {
     let mut context = tera::Context::new();
     context.insert("name", &"John Doe");
-
+    println!("through index");
     let rendered = tera.render("index.html", &context);
     match rendered {
-        //
+        Ok(..) => HttpResponse::Ok().body(rendered.unwrap()),
+        // TODO: Show the error message.
+        Err(..) => panic!("Tera rendering could not execute.")
+    }
+    // HttpResponse::Ok().body(rendered.unwrap())
+}
+
+#[get("introduce")]
+pub async fn introduce(tera: Data<Tera>) -> impl Responder {
+    let mut context = tera::Context::new();
+    println!("through");
+    let rendered = tera.render("introduce.html", &context);
+    match rendered {
+        Ok(..) => HttpResponse::Ok().body(rendered.unwrap()),
+        // TODO: Show the error message.
+        Err(..) => panic!("Tera rendering could not execute.")
+    }
+    // HttpResponse::Ok().body(rendered.unwrap())
+}
+#[get("products")]
+pub async fn products(tera: Data<Tera>) -> impl Responder {
+    let mut context = tera::Context::new();
+    let rendered = tera.render("products.html", &context);
+    match rendered {
+        Ok(..) => HttpResponse::Ok().body(rendered.unwrap()),
+        // TODO: Show the error message.
+        Err(..) => panic!("Tera rendering could not execute.")
+    }
+    // HttpResponse::Ok().body(rendered.unwrap())
+}
+
+#[get("skills")]
+pub async fn skills(tera: Data<Tera>) -> impl Responder {
+    let mut context = tera::Context::new();
+    let rendered = tera.render("skills.html", &context);
+    match rendered {
         Ok(..) => HttpResponse::Ok().body(rendered.unwrap()),
         // TODO: Show the error message.
         Err(..) => panic!("Tera rendering could not execute.")
